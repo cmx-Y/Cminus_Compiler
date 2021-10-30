c95a5690cf375c74827cd6d88882ba41a72aa2e8
dormant <dormant@mail.ustc.edu.cn>
2021-10-30 06:05:05 -0700

    first change

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -18,15 +18,62 @@
  * scope.find: find and return the value bound to the name
  */
 
-void CminusfBuilder::visit(ASTProgram &node) {
-	node.declarations->accept(*this);
- }
+void CminusfBuilder::visit(ASTProgram &node) { 
+    if(node.declarations.size == 0)                         //没有定义，出错
+        exit;
+    for (auto cur_declaration : node.declarations)          //遍历node.declarations中的所有元素
+    {
+        cur_declaration->accept(*this);
+    }
+}
 
-void CminusfBuilder::visit(ASTNum &node) { }
+void CminusfBuilder::visit(ASTNum &node) { 
+    switch (node.type)                                      //根据num的类型得到常数的值
+    {
+    case TYPE_INT:                                          //val定义在CminusfBuilder类中
+        val = ConstantInt::get(node.i_val, module.get());   //CONST_INT(node.i_val)
+        break;
+    case TYPE_FLOAT:
+        val = CONST_FP(node.f_val);
+        break;
+    default:
+        exit;
+    }
+}
 
-void CminusfBuilder::visit(ASTVarDeclaration &node) { }
+void CminusfBuilder::visit(ASTVarDeclaration &node) { 
+    auto *vartype, *varTy;
+    if(node.type == TYPE_INT)
+        vartype = Type::get_int32_type(module.get());
+    else if(node.type == TYPE_FLOAT)
+        vartype = Type::get_float_type(module.get());
+    if(node.num == nullptr)                                     //说明产生式为var-declaration →type-specifier ID ;
+        varTy = vartype;                                        
+    else                                                        //产生式为type-specifier ID [ INTEGER ] ;
+        varTy = ArrayType::get(vartype, node.num);              //定义数组类型
+     
+    if(builder->get_insert_block){                              //访问者在某个模块中
+        auto var = builder->create_alloca(varTy);
+    }
+    else{                                                       //全局变量
+        auto var = GlobalVariable::create(node.id, module.get(), varTy, false, CONST_ZERO(varTy));
+    }    
+    
+    scope.push(node.id, var);
+}
 
-void CminusfBuilder::visit(ASTFunDeclaration &node) { }
+void CminusfBuilder::visit(ASTFunDeclaration &node) { 
+    auto *retTy;                                                //返回值类型
+    if(node.type == TYPE_INT)
+        retTy = Type::get_int32_type(module.get());
+    else if(node.type == TYPE_FLOAT)
+        retTy = Type::get_float_type(module.get());
+    else
+        retTy = Type::get_void_type(module.get());
+    
+    //为了定义函数类型，需要先得到参数类型列表
+    for()
+}
 
 void CminusfBuilder::visit(ASTParam &node) { }
 
